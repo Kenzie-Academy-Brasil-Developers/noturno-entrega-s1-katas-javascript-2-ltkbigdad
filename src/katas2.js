a1986ecdcf7f957c5aa27829613c494f34f4397a
Douglas Braz Batista <doug.batista@live.com>
2022-02-12 19:49:01 -0300

    src/katas2.js

--- src/katas2.js
+++ src/katas2.js
@@ -1,25 +1,44 @@
 // comece a criar a sua função add na linha abaixo
+function add (num1, num2){
+    let soma = num1 + num2;
+    return soma;
+    
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+//console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
 
-
+function multiply (soma, num2){
+    let produto = 0;
+    for (let i = 0; i < soma; i++){
+      produto += num2;
+    }
+    return produto;
+  }
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+ //console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-
+function power (soma, produto){
+    let result = soma;
+    for (let i = 1; i < produto; i++){
+      result += result;
+    }
+    return result;
+  }
 
 // descomente a linha seguinte para testar sua função
 // console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+function factorial ( ){
+    
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
@@ -34,3 +53,6 @@
 
 // descomente a linha seguinte para testar sua função
 // console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+
+
+
